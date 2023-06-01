9d1c2a646948ebcae4f98b23107eccf41d946955
yue-pu <117700148+yue-pu@users.noreply.github.com>
2023-06-01 17:43:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,45 +4,45 @@ import quart
 import quart_cors
 from quart import request
 
-app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
+app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com") # 允许chatgpt官方domin的访问
 
 # Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
 
-@app.post("/todos/<string:username>") #chatgpt发送todo数据到plugin
-async def add_todo(username):
+@app.post("/todos/<string:username>") # chatgpt使用POST请求发送的todo数据给plugin.这里plugin响应
+async def add_todo(username): # 添加TODO函数
     request = await quart.request.get_json(force=True)
     if username not in _TODOS:
         _TODOS[username] = []
     _TODOS[username].append(request["todo"])
     return quart.Response(response='OK', status=200)
 
-@app.get("/todos/<string:username>") #chatgpt从plugin取得todo数据
-async def get_todos(username):
+@app.get("/todos/<string:username>") #chatgpt使用GET请求从plugin取得todo数据, 这里plugin响应请求
+async def get_todos(username): # 响应函数
     return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
 
-@app.delete("/todos/<string:username>") #chatgpt删除plugin中的todo数据
+@app.delete("/todos/<string:username>") #chatgpt删除plugin中的todo数据, 这里plugin响应请求
 async def delete_todo(username):
     request = await quart.request.get_json(force=True)
     todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
+    # fail silently, it's a simple plugin 判断是否已经有数据
     if 0 <= todo_idx < len(_TODOS[username]):
         _TODOS[username].pop(todo_idx)
     return quart.Response(response='OK', status=200)
 
-@app.get("/logo.png")
+@app.get("/logo.png")#响应读取logo的请求
 async def plugin_logo():
     filename = 'logo.png'
     return await quart.send_file(filename, mimetype='image/png')
 
-@app.get("/.well-known/ai-plugin.json")
+@app.get("/.well-known/ai-plugin.json")#响应读取manifest文件的请求
 async def plugin_manifest():
     host = request.headers['Host']
     with open("./.well-known/ai-plugin.json") as f:
         text = f.read()
         return quart.Response(text, mimetype="text/json")
 
-@app.get("/openapi.yaml")
+@app.get("/openapi.yaml")#响应读取openAPI规范文件(仕様書)的请求
 async def openapi_spec():
     host = request.headers['Host']
     with open("openapi.yaml") as f:
@@ -50,7 +50,7 @@ async def openapi_spec():
         return quart.Response(text, mimetype="text/yaml")
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    app.run(debug=True, host="0.0.0.0", port=5003)#启动服务
 
 if __name__ == "__main__":
     main()
